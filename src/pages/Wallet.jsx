aa3757755071615cf6d3a8304d7fd7ec65e9b07b
John Nash <john@redd.ink>
2024-11-20 09:38:19 +1100

    use TransactionHistoryComponent

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,13 +1,36 @@
 import {useState, useEffect} from 'react';
 import {useElectrum} from '../context/ElectrumContext.jsx';
+import TransactionHistoryComponent from '../components/TransactionHistoryComponent.jsx';
+
+const loadTransactions = async (wallet, setTransactions) => {
+  try {
+    const transactions = await wallet.getTransactions();
+    setTransactions(transactions);
+  } catch (error) {
+    console.error('Failed to fetch transactions', error);
+  }
+};
 
 const Wallet = () => {
   const {walletStore, wallet} = useElectrum();
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    if (wallet) {
+      loadTransactions(wallet, setTransactions);
+    }
+  }, [wallet]);
 
   useEffect(() => {
     // Additional useEffect to watch for changes in walletStore
     // No specific action needed, rerender will be triggered automatically
   }, [walletStore]);
+
+  const getTransactions = () => {
+    if (!wallet) return [];
+    const transactions = wallet.getTransactions();
+    return transactions;
+  }
 
   const handleRestoreButtonClick = () => {
     setIsOpen(true);
@@ -29,6 +52,7 @@ const Wallet = () => {
               <header>
                 <h1>Reddcoin Wallet</h1>
               </header>
+              <TransactionHistoryComponent data={getTransactions()}/>
             </>
         ) : (
             <>
